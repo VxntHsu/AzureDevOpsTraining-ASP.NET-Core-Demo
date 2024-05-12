f5d0ababd37a65f2733887325569c039fda632e8
Duran Hsieh <dog0416@gmail.com>
2024-05-12 22:04:47 +0800

    Update UnitTest1.cs

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -6,11 +6,17 @@ namespace UnitTests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        //[TestMethod]
         public void TestMethod1()
         {
             var url = Environment.GetEnvironmentVariable("URL", EnvironmentVariableTarget.Process);
             Assert.AreEqual("https://study4.tw", url);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("1", "1");
         }
     }
 }
